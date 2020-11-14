17bbeddbe14bbd070b543198803ca4a4f7ce9df0
juleka <jule@hrdag.org>
2020-11-14 11:58:27 -0500

    removing false stop check

--- clean/src/uncode-conflict-years.R
+++ clean/src/uncode-conflict-years.R
@@ -13,20 +13,18 @@ parser$add_argument("--outputfile", type="character")
 arguments <- parser$parse_args()
 
 data <- read.csv(arguments$inputfile, header=TRUE, sep='|', stringsAsFactors = FALSE)
+print(table(data$conflictyear, useNA="always"))
 
 #recode SVAC conflict years, if not in UCDP/PRIO set
 not_conflict_years <- read.csv(arguments$recode_conflict_yrs, header=TRUE, sep = '|', stringsAsFactors = FALSE)
 not_conflict_years$conflict_country <- NULL
 not_conflict_years$recode_conflictyear <- 1
-recode.check <- nrow(not_conflict_years)
-svac.cy.check <- nrow(data$conflictyear==1)
 
 data <- merge(data, not_conflict_years, by=c('conflictid_new', 'year'), all.x = TRUE)
 data$conflictyear <- ifelse(data$recode_conflictyear==1 & !is.na(data$recode_conflictyear), 0, data$conflictyear)
-stopifnot(svac.cy.check - nrow(data$conflictyear==1) == recode.check)
 data$recode_conflictyear <- NULL
-print(table(data$conflictyear, useNA="always"))
 
+print(table(data$conflictyear, useNA="always"))
 write.table(data, arguments$outputfile, row.names=FALSE, sep='|', quote=FALSE)
 print(paste("done printing data to", arguments$outputfile))
 # end of Rscript.
